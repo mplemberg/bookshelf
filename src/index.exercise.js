3faa866f40ed6d0ac1b91c433635269968991230
Mark Lemberg <mark.lemberg@redlion.net>
2024-04-11 12:16:07 -0400

    Completing Boostrap Extra Credit 1 - adding login/register modals

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,47 @@
-// 🐨 you'll need to import react and createRoot from react-dom up here
+import * as React from 'react'
+import {createRoot} from 'react-dom/client'
+import {Logo} from './components/logo'
+import '@reach/dialog/styles.css'
+import {Dialog} from '@reach/dialog'
+function App() {
+  const MODAL_TYPES = {
+    LOGIN: 'LOGIN',
+    REGISTER: 'REGISTER',
+  }
+  const [openModal, setOpenModal] = React.useState('')
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
+  const isOpen = dialogType => openModal === dialogType
+  const close = () => setOpenModal('')
+  return (
+    <div>
+      <Logo width="80" height="80" />
+      <h1>Bookshelf</h1>
+      <div>
+        <button onClick={() => setOpenModal(MODAL_TYPES.LOGIN)}>Login</button>
+      </div>
+      <div>
+        <button onClick={() => setOpenModal(MODAL_TYPES.REGISTER)}>
+          Register
+        </button>
+      </div>
+      <Dialog
+        aria-label="Login"
+        isOpen={isOpen(MODAL_TYPES.LOGIN)}
+        onDismiss={close}
+      >
+        <h1>Login</h1>
+      </Dialog>
+      <Dialog
+        aria-label="Register"
+        isOpen={isOpen(MODAL_TYPES.REGISTER)}
+        onDismiss={close}
+      >
+        <h1>Register</h1>
+      </Dialog>
+    </div>
+  )
+}
 
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
-
-// 🐨 use createRoot to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
+export {root}
