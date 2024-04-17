958d8d7b67eb82d90c8244143afb64affd9f8655
Mark Lemberg <mplemberg@gmail.com>
2024-04-17 10:56:21 -0400

    switch to useAsync

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -3,35 +3,71 @@ import {jsx} from '@emotion/core'
 
 import * as React from 'react'
 import * as auth from 'auth-provider'
+import {client} from './utils/api-client'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
-import {client} from 'utils/api-client.exercise'
+import {useAsync} from 'utils/hooks'
+import {FullPageSpinner} from 'components/lib'
+import * as colors from 'styles/colors'
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
+
+function ErrorDisplay({error}) {
+  return (
+    <div
+      css={{
+        color: colors.danger,
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <p>Uh oh... There's a problem. Try refreshing the app.</p>
+      <pre>{error.message}</pre>
+    </div>
+  )
+}
 
 function App() {
-  const [user, setUser] = React.useState(null)
+  const {
+    data: user,
+    error,
+    isIdle,
+    isLoading,
+    isSuccess,
+    isError,
+    run,
+    setData: setUser,
+  } = useAsync()
+
+  React.useEffect(() => {
+    run(getUser())
+  }, [run])
 
   const login = form => auth.login(form).then(u => setUser(u))
   const register = form => auth.register(form).then(u => setUser(u))
+
   const logout = () => {
     auth.logout()
     setUser(null)
   }
 
-  async function getUser() {
-    let user = null
-    const token = await auth.getToken()
-    if (token) {
-      const data = await client('me', {token})
-      user = data.user
-    }
-    return user
-  }
-
-  React.useEffect(() => {
-    getUser().then(u => setUser(u))
-  }, [])
-
-  return user ? (
+  return isLoading || isIdle ? (
+    <FullPageSpinner />
+  ) : isError ? (
+    <ErrorDisplay error={error} />
+  ) : user ? (
     <AuthenticatedApp user={user} logout={logout} />
   ) : (
     <UnauthenticatedApp login={login} register={register} />
