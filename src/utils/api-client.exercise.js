640b1c2ee0585085e2cc86f9eb54788c1ae5ae7a
Mark Lemberg <mplemberg@gmail.com>
2024-04-17 11:13:15 -0400

    support POST in API client

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -3,19 +3,24 @@ const apiURL = process.env.REACT_APP_API_URL
 
 function client(
   endpoint,
-  {token, headers: customHeaders, ...customConfig} = {},
+  {token, headers: customHeaders, data, ...customConfig} = {},
 ) {
   const config = {
+    method: 'GET',
     headers: {
       Authorization: token ? `Bearer ${token}` : undefined,
+      'Content-Type':
+        customConfig.method === 'POST' ? 'application/json' : undefined,
       ...customHeaders,
     },
+    body: customConfig.method === 'POST' ? JSON.stringify(data) : undefined,
     ...customConfig,
   }
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
     if (response.status === 401) {
       await auth.logout()
+      // refresh the page for them
       window.location.assign(window.location)
       return Promise.reject({message: 'Please re-authenticate.'})
     }
