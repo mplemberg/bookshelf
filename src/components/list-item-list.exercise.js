bb7a7c678e441554d1bf6d698442f102599f1ebf
Mark Lemberg <mplemberg@gmail.com>
2024-04-25 10:59:02 -0400

    wire everything up with React Query

--- src/components/list-item-list.exercise.js
+++ src/components/list-item-list.exercise.js
@@ -1,8 +1,8 @@
 /** @jsx jsx */
 import {jsx} from '@emotion/core'
 
-// 🐨 you'll need useQuery from 'react-query'
-// 🐨 and client from 'utils/api-client'
+import {useQuery} from 'react-query'
+import {client} from 'utils/api-client'
 import {BookListUL} from './lib'
 import {BookRow} from './book-row'
 
@@ -15,25 +15,34 @@ function ListItemList({
   // 🐨 call useQuery to get the list-items from the 'list-items' endpoint
   // queryKey should be 'list-items'
   // queryFn should call the 'list-items' endpoint
-
-  // 🐨 assign this to the list items you get back from react-query
-  const listItems = null
+  const {data: listItems, isLoading} = useQuery({
+    queryKey: 'list-items',
+    queryFn: () =>
+      client('list-items', {token: user.token}).then(data => data.listItems),
+  })
 
   const filteredListItems = listItems?.filter(filterListItems)
 
   if (!listItems?.length) {
-    return <div css={{marginTop: '1em', fontSize: '1.2em'}}>{noListItems}</div>
+    return (
+      <div css={{marginTop: '1em', fontSize: '1.2em'}}>
+        {noListItems} {isLoading ? <div>Loading...</div> : null}
+      </div>
+    )
   }
   if (!filteredListItems.length) {
     return (
       <div css={{marginTop: '1em', fontSize: '1.2em'}}>
         {noFilteredListItems}
+        {isLoading ? <div>Loading...</div> : null}
       </div>
     )
   }
 
   return (
     <BookListUL>
+      {isLoading ? <div>Loading...</div> : null}
+
       {filteredListItems.map(listItem => (
         <li key={listItem.id}>
           <BookRow user={user} book={listItem.book} />
